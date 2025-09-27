ea95b152db7928d02b3378d5ac500847358d29ea
Insar <109831437+Insar2005@users.noreply.github.com>
2025-09-27 13:30:13 +0000

    update

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from models import init_db
 import requests as rq
 
 
+class AddTask(BaseModel):
+    tg_id: int
+    title: str
+
+class CompleteTask(BaseModel):
+    id:int
+
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -40,3 +47,14 @@ async def profile(tg_id: int):
     user = await rq.add_user(tg_id)
     completed_tasks_count= await rq.get_completed_tasks_count(user.id)
     return {'completedTasks': completed_tasks_count}
+
+@app.post("/api/add")
+async def add_task(task: AddTask):
+    user = await rq.add_user(task.tg_id)
+    await rq.add_task(user.id, task.title)
+    return {"status": "ok"}
+
+@app.patch("/api/completed")
+async def complete_Task(task: CompleteTask):
+    await rq.update_task(task.id)
+    return {"status":"ok"}
